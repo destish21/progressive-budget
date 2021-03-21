af2f6a98d7eb802e8fc2357e7e4f849be2736305
desta mulu <destish21@yahoo.com>
2021-03-21 17:47:49 -0500

    added routes to connect  mongodbAtls

--- server.js
+++ server.js
@@ -1,35 +1,38 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const compression = require('compression');
-const logger = require('morgan')
+const logger = require('morgan');
+require('dotenv').config();
 
-const PORT = process.env.PORT || 3000;
+
+const PORT = process.env.PORT;
 
 const app = express();
 
- app.use(logger('dev'));
+app.use(logger('dev'));
 
-  app.use(compression());
+app.use(compression());
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static('public'));
 
 const uri = process.env.MONGODB_URI
 
-mongoose.connect(uri ,
-   {
-       useNewUrlParser: true,
-       useUnifiedTopology: true,
-       useCreateIndex: true,
-       useFindAndModify: false 
+mongoose.connect(uri,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    }).then(() => console.log('mongodb connected'))
+    .catch(error => console.log('mongodb connection', error));
 
-});
- 
 // routes
 app.use(require('./routes/api.js'));
 
-app.listen(PORT, ()=>{
+// linsing ports
+app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!!`);
 })
